b59ed32c0477cf406565e29f2c6b76d46b984010
Samar pawase <samarpawase@gmail.com>
2022-01-30 16:38:55 +0530

    number change

--- 04 Integerlength.py
+++ 04 Integerlength.py
@@ -1,14 +1,17 @@
 # Given an integer return the number of digits in it
 
 import math
+
+
 def numberOfDigits(number):
-    if (number ==0):
+    if (number == 0):
         return 1
     elif(number < 0):
         return int(math.log10(-1*number)+1)
     return int(math.log10(number)+1)
 
+
 print(numberOfDigits(0))
 print(numberOfDigits(000))
 print(numberOfDigits(100))
-print(numberOfDigits(-23))
+print(numberOfDigits(-2332))
